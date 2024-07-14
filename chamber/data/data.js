f6cc07b70e28c681e29453a535048743e8d74b4a
natanri <riosjhonatan44@gmail.com>
2024-07-13 22:57:27 -0500

    Discover page

--- chamber/data/data.js
+++ chamber/data/data.js
@@ -18,11 +18,34 @@ document.addEventListener("DOMContentLoaded", async function(){
             menuList.classList.remove('show');
             menuList.style.display = 'flex';
         } else {
-            menuList.style.display = 'block';            
+            menuList.style.display = 'none';            
         }
     }
 
     window.addEventListener('resize', handleResize);
-    handleResize();     
+    handleResize(); 
+    
+    //visit message
+    const visitMessage = document.getElementById('visitMessage');
+    const lastVisit = localStorage.getItem("lastVisit");
+    const currenTime = new Date().getTime();
+
+    if(lastVisit){
+        const lastVisitDate = new Date(parseInt(lastVisit));
+        const timeDiff = currenTime - lastVisitDate.getTime();
+        const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+        if (lastVisit){
+            visitMessage.textContent = 'Back so soon! Awesome';
+        } else if(daysDiff == 1) {
+            visitMessage.textContent = `You last visited 1 day ago`;
+        } else{
+            visitMessage.textContent = `You last visited ${daysDiff} days ago.`;
+        }
+    }else{
+        visitMessage.textContent = "Welcome! Let us know if you have any questions."
+    }
+    
+    localStorage.setItem("lastVisit", currenTime.toString());
     
 });
